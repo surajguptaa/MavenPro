764ae4586b9b9cf64daac1a00a0335ec6d6136b6
Suraj Gupta <109907553+surajguptaa@users.noreply.github.com>
2023-06-11 21:58:27 +0530

    Create MailSendData.java

---
+++ emailSender/src/main/java/org/example/MailSendData.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class MailSendData {
+    public static final String HostServer = "smtp.gmail.com";
+    public static final String port = "465";
+    public static final String sslProperty = "gmail.smtp.ssl.enable";
+    public static final String authPerm = "gmail.smtp.auth";
+    public static final String myUserMail = "surajwap16@gmail.com";
+    public static final String password = "tthpxgbfaebfjzpr"; //app password
+    public static final String receiverMail = "stechenquiry@gmail.com";
+
+
+}
